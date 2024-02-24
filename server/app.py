2cc4e0c1e8cce86e4615a3fea064ef7a0c79ac49
nematatu <nematatu1003@gmail.com>
2024-02-25 02:45:21 +0900

    fix main.yml

--- server/app.py
+++ server/app.py
@@ -5,7 +5,7 @@ app.json.ensure_ascii=False
 
 @app.route("/")
 def hello():
-    return "Hello, World!!!!!!!!"
+    return "Hello, Worlddayo---n!!!!!server/!!"
 
 if __name__ == '__main__':
     app.run()
